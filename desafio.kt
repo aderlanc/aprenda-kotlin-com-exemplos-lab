dc0e28b648bbc263dece3f16f162f2e12cb55958
Aderlan <61508683+aderlanc@users.noreply.github.com>
2023-12-27 16:55:49 -0300

    Desafio de código

--- desafio.kt
+++ desafio.kt
@@ -1,20 +1,3 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
-enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
-
-class Usuario
-
-data class ConteudoEducacional(vaL nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
-}
-
 fun main() {
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
@@ -40,15 +23,10 @@ fun main() {
 
     val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", duracao = 30)
     val conteudo2 = ConteudoEducacional("Kotlin Avançado", duracao = 120)
+
     val nivel = Nivel.BASICO
     
-    println(nivel)
-
     val formacao = Formacao("Kotlin para backend", nivel, listOf(conteudo1, conteudo2))
-    
-    println(formacao)
-    
-    println("Formação: ${formacao.nome}")
 
     formacao.matricular(usuario1)
     formacao.matricular(usuario2)
