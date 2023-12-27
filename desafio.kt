d00487efdc3de92b0b84e05479e25a9ff09e04c1
Aderlan <61508683+aderlanc@users.noreply.github.com>
2023-12-27 17:00:50 -0300

    Desafio de código

--- desafio.kt
+++ desafio.kt
@@ -1,8 +1,3 @@
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
-
 enum class Nivel{BASICO, INTERMEDIARIO, AVANCADO}
 
 class Usuario(val nome: String)
